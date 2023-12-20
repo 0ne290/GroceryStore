fdc339268bb207c85969f93d69ee1bcd4e7cc425
Алексей <97665299+0ne290@users.noreply.github.com>
2023-12-20 21:51:23 +0800

    Work

---
+++ GroceryStore.Logic/Dto/CountryDto.cs
@@ -0,0 +1,12 @@
+namespace GroceryStore.Logic.Dto;
+
+public class CountryDto
+{
+    public int Key { get; set; }
+
+    public string Name { get; set; }
+
+    //public IEnumerable<IManufacturer> Manufacturers { get; set; } = new List<IManufacturer>();
+
+    //public IEnumerable<IRegion> Regions { get; set; } = new List<IRegion>();
+}
