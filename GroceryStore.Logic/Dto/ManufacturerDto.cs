93c873e0d574e7a7c4e495af11b093935ad9769e
Алексей <97665299+0ne290@users.noreply.github.com>
2023-12-20 21:54:46 +0800

    Work

---
+++ GroceryStore.Logic/Dto/ManufacturerDto.cs
@@ -0,0 +1,34 @@
+namespace GroceryStore.Logic.Dto;
+
+public class ManufacturerDto
+{
+    public int Key { get; set; }
+
+    public string Name { get; set; }
+
+    public string Contact { get; set; }
+
+    public int CountryKey { get; set; }
+
+    public int RegionKey { get; set; }
+
+    public int CityKey { get; set; }
+
+    public int StreetKey { get; set; }
+
+    public int Postcode { get; set; }
+
+    public int HouseNumber { get; set; }
+
+    public string HouseLetter { get; set; }
+    
+    //public ICountry? CountryKeyNavigation { get; set; }
+    
+    //public IRegion? RegionKeyNavigation { get; set; }
+    
+    //public ICity? CityKeyNavigation { get; set; }
+
+    //public IStreet? StreetKeyNavigation { get; set; }
+
+    //public ICollection<IProduct> Products { get; set; } = new List<IProduct>();
+}
