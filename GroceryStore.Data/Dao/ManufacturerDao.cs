9469cd4d30eb6c55f22911007fdc50b4b7d1f40b
Алексей <97665299+0ne290@users.noreply.github.com>
2023-12-20 21:44:30 +0800

    Work

---
+++ GroceryStore.Data/Dao/ManufacturerDao.cs
@@ -0,0 +1,45 @@
+using GroceryStore.Data.Entities;
+using GroceryStore.Data.Interfaces;
+using GroceryStore.Data.NullObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroceryStore.Data.Dao;
+
+public class ManufacturerDao
+{
+    public ManufacturerDao(IGroceryStoreContext dbContext) => _dbContext = dbContext;
+
+    public void Create(Manufacturer manufacturer) => _dbContext.Manufacturers.Add(manufacturer);
+
+    public IEnumerable<Manufacturer> GetAll() => _dbContext.Manufacturers.AsNoTracking();
+
+    public IManufacturer GetByKey(int key)
+    {
+        var manufacturer = _dbContext.Manufacturers.Find(key);
+        
+        if (manufacturer is null)
+            return new NullManufacturer();
+
+        return manufacturer;
+    }
+
+    public void Update(Manufacturer manufacturer) => _dbContext.Manufacturers.Update(manufacturer);
+
+    public void Remove(Manufacturer manufacturer) => _dbContext.Manufacturers.Remove(manufacturer);
+
+    public bool SaveChanges()
+    {
+        try
+        {
+            _dbContext.SaveChanges();
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private readonly IGroceryStoreContext _dbContext;
+}
