16dc825b98143adb5676c9d4c194ba719176b190
Алексей <97665299+0ne290@users.noreply.github.com>
2023-12-22 22:17:17 +0800

    Work

--- GroceryStore.Logic/Dto/BaseDto.cs
+++ GroceryStore.Logic/Dto/BaseDto.cs
@@ -4,7 +4,7 @@ public abstract class BaseDto
 {
     protected BaseDto(int[] primaryKey) => PrimaryKey = primaryKey;
 
-    public bool IsEmpty() => PrimaryKey.Any(key => key == -1);
+    public virtual bool IsEmpty() => PrimaryKey.Any(key => key == -1);
     
     protected int[] PrimaryKey { get; set; }
 }
