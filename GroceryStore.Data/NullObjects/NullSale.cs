1eb8eb90ebe96e83545ef297c16c6ce4bcf076f3
Алексей <97665299+0ne290@users.noreply.github.com>
2023-12-22 22:14:30 +0800

    Work

---
+++ GroceryStore.Data/NullObjects/NullSale.cs
@@ -0,0 +1,18 @@
+using GroceryStore.Data.Interfaces;
+
+namespace GroceryStore.Data.NullObjects;
+
+public class NullSald : ISale
+{
+    public int ProductKey => -1;
+
+    public int CustomerKey => -1;
+
+    public DateTime Date => DateTime.UnixEpoch;
+
+    public int? Quantity => -1;
+
+    public IRegularCustomer CustomerKeyNavigation => new NullRegularCustomer();
+
+    public IProduct ProductKeyNavigation => new NullProduct();
+}
