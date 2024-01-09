cc78acd4830d5f3b70d409fcb48bb9f726b2f122
Алексей <97665299+0ne290@users.noreply.github.com>
2024-01-09 23:55:23 +0800

    Update Mapper.cs

--- GroceryStore.Data/Mapper.cs
+++ GroceryStore.Data/Mapper.cs
@@ -11,6 +11,8 @@ public class Mapper
     {
         CityDto cityDto => CityDtoToCity(cityDto),
         CountryDto countryDto => CountryDtoToCountry(countryDto),
+        EmployeeDto employeeDto => EmployeeDtoToEmployee(employeeDto),
+        ManufacturerDto manufacturerDto => ManufacturerDtoToManufacturer(manufacturerDto),
         _ => throw new ArgumentException($"There is no corresponding entity in the database for the DTO class \"{dto}\"")
     };
     
@@ -23,6 +25,27 @@ public class Mapper
     private Country CountryDtoToCountry(CountryDto countryDto) => new Country()
     {
         Key = countryDto.Key, Name = countryDto.Name == "NullName" ? null : countryDto.Name
+    };
+
+    private Employee EmployeeDtoToEmployee(EmployeeDto employeeDto) => new Employee()
+    {
+        Key = employeeDto.Key, FullName = employeeDto.FullName == "NullFullName" ? null : employeeDto.FullName,
+        StoreKey = employeeDto.StoreKey == -1 ? null : employeeDto.StoreKey,
+        PositionKey = employeeDto.PositionKey == -1 ? null : employeeDto.PositionKey,
+        EmploymentDate = employeeDto.EmploymentDate == default(DateTime) ? null : employeeDto.EmploymentDate
+    };
+
+    private Manufacturer ManufacturerDtoToManufacturer(ManufacturerDto manufacturerDto) => new Manufacturer()
+    {
+        Key = manufacturerDto.Key, Name = manufacturerDto.Name == "NullName" ? null : manufacturerDto.Name,
+        Contact = manufacturerDto.Contact == "NullContact" ? null : manufacturerDto.Contact,
+        CountryKey = manufacturerDto.CountryKey == -1 ? null : manufacturerDto.CountryKey,
+        RegionKey = manufacturerDto.RegionKey == -1 ? null : manufacturerDto.RegionKey,
+        CityKey = manufacturerDto.CityKey == -1 ? null : manufacturerDto.CityKey,
+        StreetKey = manufacturerDto.StreetKey == -1 ? null : manufacturerDto.StreetKey,
+        Postcode = manufacturerDto.Postcode == -1 ? null : manufacturerDto.Postcode,
+        HouseNumber = manufacturerDto.HouseNumber == -1 ? null : manufacturerDto.HouseNumber,
+        HouseLetter = manufacturerDto.HouseLetter == "NullHouseLetter" ? null : manufacturerDto.HouseLetter
     };
     
     public IDto EntityToDto(IEntity entity) => entity switch
